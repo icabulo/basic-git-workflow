7ded8592ab2b4abbc34f603d3b6f2988d64090c5
Ivan <icabulo@gmail.com>
2023-03-04 09:36:10 -0500

    Add javascript to code

---
+++ script.js
@@ -0,0 +1,7 @@
+//Wait for windos to load
+document.addEventListener("DOMContentLoaded",function (event){
+	//get reference to header object
+	let myHeader =document.getElementbyID("header-id)
+
+	//wait
+});
